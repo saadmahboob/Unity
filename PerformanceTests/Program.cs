2169dc9f15a971dfbcf8a92909f7de7320bb63e0
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-07-11 22:47:37 -0400

    Modifications to the test

--- PerformanceTests/Program.cs
+++ PerformanceTests/Program.cs
@@ -11,16 +11,34 @@ using TestUtils;
 
 namespace PerformanceTests
 {
+    public class BuildPerformanceTestOptions
+    {
+        public BuildPerformanceTestOptions(int secondaryFileRetainEvery = 2, int secondaryFolderCollapseEvery = 4, int secondaryFilePerFolderCount = 10, int secondaryFolderCount = 10, int initialFilePerFolderCount = 10, int initialFolderCount = 10)
+        {
+            SecondaryFileRetainEvery = secondaryFileRetainEvery;
+            SecondaryFolderCollapseEvery = secondaryFolderCollapseEvery;
+            SecondaryFilePerFolderCount = secondaryFilePerFolderCount;
+            SecondaryFolderCount = secondaryFolderCount;
+            InitialFilePerFolderCount = initialFilePerFolderCount;
+            InitialFolderCount = initialFolderCount;
+        }
+
+        public int SecondaryFileRetainEvery { get; }
+        public int SecondaryFolderCollapseEvery { get; }
+        public int SecondaryFilePerFolderCount { get; }
+        public int SecondaryFolderCount { get; }
+        public int InitialFilePerFolderCount { get; }
+        public int InitialFolderCount { get; }
+    }
+
     public class TreeBuilderTests
     {
         private IEnvironment environment;
         private Action<FileTreeNode> stateChangeCallback;
         private GitObjectFactory gitObjectFactory;
 
-        private IList<GitStatusEntry> firstEntryDataSet;
-
-        private IList<GitStatusEntry> secondEntryDataSet;
-        private List<string> secondEntryFoldedFolderSet;
+        private PerformanceTestDataSet basicPerformanceTestDataSet;
+        private PerformanceTestDataSet heavyPerformanceTestDataSet;
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -44,42 +62,53 @@ namespace PerformanceTests
 
             gitObjectFactory = new GitObjectFactory(environment);
 
-            firstEntryDataSet = new List<GitStatusEntry>();
+            basicPerformanceTestDataSet = BuildPerformanceTestDataSet(gitObjectFactory);
+            Console.WriteLine($@"Basic Test Set{Environment.NewLine}{basicPerformanceTestDataSet}");
 
-            secondEntryDataSet = new List<GitStatusEntry>();
-            secondEntryFoldedFolderSet = new List<string>();
+            heavyPerformanceTestDataSet = BuildPerformanceTestDataSet(gitObjectFactory, new BuildPerformanceTestOptions(initialFolderCount: 20, initialFilePerFolderCount: 20, secondaryFolderCount: 20, secondaryFilePerFolderCount: 20));
+            Console.WriteLine($@"Heavy Test Set{Environment.NewLine}{heavyPerformanceTestDataSet}");
+        }
 
-            for (var folderIndex = 0; folderIndex < 10; folderIndex++)
+        private static PerformanceTestDataSet BuildPerformanceTestDataSet(GitObjectFactory gitObjectFactory, BuildPerformanceTestOptions options = null)
+        {
+            options = options ?? new BuildPerformanceTestOptions();
+
+            var firstEntryDataSet = new List<GitStatusEntry>();
+            var secondEntryDataSet = new List<GitStatusEntry>();
+            var secondEntryFoldedFolderSet = new List<string>();
+
+            for (var folderIndex = 0; folderIndex < options.InitialFolderCount; folderIndex++)
             {
                 var folderName = $"folder-{folderIndex}";
                 var folder = folderName.ToNPath();
 
-                if (folderIndex % 2 == 0)
+                if (folderIndex % options.SecondaryFolderCollapseEvery == 0)
                 {
                     secondEntryFoldedFolderSet.Add(folder);
                 }
 
-                for (var fileIndex = 0; fileIndex < 10; fileIndex++)
+                for (var fileIndex = 0; fileIndex < options.InitialFilePerFolderCount; fileIndex++)
                 {
                     var file = $"{folderName}-file-{fileIndex}.txt".ToNPath();
                     var metafile = $"{folderName}-file-{fileIndex}.txt.meta".ToNPath();
-                    
+
                     firstEntryDataSet.Add(gitObjectFactory.CreateGitStatusEntry(folder.Combine(file), GitFileStatus.Added));
                     firstEntryDataSet.Add(gitObjectFactory.CreateGitStatusEntry(folder.Combine(metafile), GitFileStatus.Added));
 
-                    if (fileIndex % 2 == 0)
+                    if (fileIndex % options.SecondaryFileRetainEvery == 0)
                     {
                         secondEntryDataSet.Add(gitObjectFactory.CreateGitStatusEntry(folder.Combine(file), GitFileStatus.Added));
+                        secondEntryDataSet.Add(gitObjectFactory.CreateGitStatusEntry(folder.Combine(metafile), GitFileStatus.Added));
                     }
                 }
             }
 
-            for (var folderIndex = 20; folderIndex < 30; folderIndex++)
+            for (var folderIndex = options.InitialFolderCount; folderIndex < options.InitialFolderCount + options.SecondaryFolderCount; folderIndex++)
             {
                 var folderName = $"folder-{folderIndex}";
                 var folder = folderName.ToNPath();
 
-                for (var fileIndex = 0; fileIndex < 10; fileIndex++)
+                for (var fileIndex = 0; fileIndex < options.SecondaryFilePerFolderCount; fileIndex++)
                 {
                     var file = $"{folderName}-file-{fileIndex}.txt".ToNPath();
                     var metafile = $"{folderName}-file-{fileIndex}.txt.meta".ToNPath();
@@ -88,26 +117,68 @@ namespace PerformanceTests
                     secondEntryDataSet.Add(gitObjectFactory.CreateGitStatusEntry(folder.Combine(metafile), GitFileStatus.Added));
                 }
             }
+
+            return new PerformanceTestDataSet(firstEntryDataSet, secondEntryDataSet, secondEntryFoldedFolderSet);
         }
 
         [Benchmark]
-        public void OriginalTreeBuilderBenchmark()
+        public void BasicOriginalTreeBuilderBenchmark()
         {
             var gitStatusEntries = new List<GitStatusEntry>();
             var gitCommitTargets = new List<GitCommitTarget>();
 
-            OriginalTreeBuilder.BuildTreeRoot(firstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
-            OriginalTreeBuilder.BuildTreeRoot(secondEntryDataSet, gitStatusEntries, gitCommitTargets, secondEntryFoldedFolderSet, stateChangeCallback);
+            OriginalTreeBuilder.BuildTreeRoot(basicPerformanceTestDataSet.FirstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
+            OriginalTreeBuilder.BuildTreeRoot(basicPerformanceTestDataSet.SecondEntryDataSet, gitStatusEntries, gitCommitTargets, basicPerformanceTestDataSet.SecondEntryFoldedFolderSet, stateChangeCallback);
         }
 
         [Benchmark]
-        public void CurrentTreeBuilderBenchmark()
+        public void BasicCurrentTreeBuilderBenchmark()
         {
             var gitStatusEntries = new List<GitStatusEntry>();
             var gitCommitTargets = new List<GitCommitTarget>();
 
-            TreeBuilder.BuildTreeRoot(firstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
-            TreeBuilder.BuildTreeRoot(secondEntryDataSet, gitStatusEntries, gitCommitTargets, secondEntryFoldedFolderSet, stateChangeCallback);
+            TreeBuilder.BuildTreeRoot(basicPerformanceTestDataSet.FirstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
+            TreeBuilder.BuildTreeRoot(basicPerformanceTestDataSet.SecondEntryDataSet, gitStatusEntries, gitCommitTargets, basicPerformanceTestDataSet.SecondEntryFoldedFolderSet, stateChangeCallback);
+        }
+        [Benchmark]
+        public void HeavyOriginalTreeBuilderBenchmark()
+        {
+            var gitStatusEntries = new List<GitStatusEntry>();
+            var gitCommitTargets = new List<GitCommitTarget>();
+
+            OriginalTreeBuilder.BuildTreeRoot(heavyPerformanceTestDataSet.FirstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
+            OriginalTreeBuilder.BuildTreeRoot(heavyPerformanceTestDataSet.SecondEntryDataSet, gitStatusEntries, gitCommitTargets, basicPerformanceTestDataSet.SecondEntryFoldedFolderSet, stateChangeCallback);
+        }
+
+        [Benchmark]
+        public void HeavyCurrentTreeBuilderBenchmark()
+        {
+            var gitStatusEntries = new List<GitStatusEntry>();
+            var gitCommitTargets = new List<GitCommitTarget>();
+
+            TreeBuilder.BuildTreeRoot(heavyPerformanceTestDataSet.FirstEntryDataSet, gitStatusEntries, gitCommitTargets, new List<string>(), stateChangeCallback);
+            TreeBuilder.BuildTreeRoot(heavyPerformanceTestDataSet.SecondEntryDataSet, gitStatusEntries, gitCommitTargets, basicPerformanceTestDataSet.SecondEntryFoldedFolderSet, stateChangeCallback);
+        }
+    }
+
+    class PerformanceTestDataSet
+    {
+        public List<GitStatusEntry> FirstEntryDataSet { get; }
+        public List<GitStatusEntry> SecondEntryDataSet { get; }
+        public List<string> SecondEntryFoldedFolderSet { get; }
+
+        public PerformanceTestDataSet(List<GitStatusEntry> firstEntryDataSet, List<GitStatusEntry> secondEntryDataSet, List<string> secondEntryFoldedFolderSet)
+        {
+            FirstEntryDataSet = firstEntryDataSet;
+            SecondEntryDataSet = secondEntryDataSet;
+            SecondEntryFoldedFolderSet = secondEntryFoldedFolderSet;
+        }
+
+        public override string ToString()
+        {
+            return $"First Count: {FirstEntryDataSet.Count}{Environment.NewLine}" +
+                $"Second Count: {SecondEntryDataSet.Count}{Environment.NewLine}" +
+                $"Second Collapsed Folder Count: {SecondEntryFoldedFolderSet.Count}";
         }
     }
 
